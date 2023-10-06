530198f33a645e72dc737a531edb30fc1d5f7cd1
hasibulhussen007 <hasibulhussen4@gmail.com>
2023-10-07 01:46:00 +0600

    add login route

---
+++ src/pages/Shared/Login/Login.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../Navbar/Navbar";
+
+
+const Login = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h2>This is log in</h2>
+        </div>
+    );
+};
+
+export default Login;
